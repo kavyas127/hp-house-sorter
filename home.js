6d21317b5d3d50059cebaaeb5f4283c014c57d3c
kavyas127 <officialkesai315@gmail.com>
2024-04-14 17:25:48 +0530

    fixing bugs

--- home.js
+++ home.js
@@ -1,28 +1,62 @@
-var qarray = ["Are you courageous?", "Are you a good leader?", "Are you kind?","Are you loyal?","Are you intelligent?","Are you creative?","Are you ambitious?","Are you patient?","Do you work hard?","Are you pure-blood?"];
-var points = -1;
+var qarray = ["Are you courageous?", "Are you a good leader?", "Are you kind?", "Are you loyal?", "Are you intelligent?", "Are you creative?", "Are you ambitious?", "Are you patient?", "Do you work hard?", "Are you pure-blood?"];
+var points = 1;
 var i = 0;
 
-$(".revealer").hide();
+function pointsdisp(points){
+    if(points >= 8 ){
+        $("h1").text("Gryffindor");
+    }
+    else if(points >= 6){
+        $("h1").text("Hufflepuff");
+    }
+    else if(points >= 4){
+        $("h1").text("Ravenclaw");
+    }
+    else{
+        $("h1").text("Slytherin");
+    }
+}
 
-$(document).ready(function(){
-    $("#yes-button").click(function(){
-        if(i < qarray.length){
+$(".revealer").hide();
+$("#submit-but").hide();
+$(".buttons-div").hide();
+$(".body-wrapper").show();
+
+$(document).ready(function () {
+    $("#start-but").click(function(){
+        $("#start-but").hide();
+        $(".buttons-div").show();
+        $("h2").text(qarray[i]);
+        i++;
+    });
+
+    $("#yes-button").click(function () {
+        if (i < qarray.length) {
             $("h2").text(qarray[i]);
             i++;
             points++;
         }
+        if (i == qarray.length) {
+            $(".buttons-div").hide();
+            $("#submit-but").show();
+        }
     });
 
-    $("#no-button").click(function(){
-        if(i < qarray.length){
+    $("#no-button").click(function () {
+        if (i < qarray.length) {
             $("h2").text(qarray[i]);
             i++;
         }
+        if (i == qarray.length) {
+            $(".buttons-div").hide();
+            $("#submit-but").show();
+        }
+    });
+
+    $("#submit-but").click(function(){
+        $(".body-wrapper").hide();
+        pointsdisp(points);
+        $(".revealer").show();
     });
 });
-
-$(".body-wrapper").hide();
-
-
-
 
