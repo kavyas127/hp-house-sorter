9b77e96d51ebf3cc0834d94cfb07e47bd22db885
kavyas127 <officialkesai315@gmail.com>
2024-04-14 17:42:43 +0530

    commit x

--- home.js
+++ home.js
@@ -1,10 +1,11 @@
 var qarray = ["Are you courageous?", "Are you a good leader?", "Are you kind?", "Are you loyal?", "Are you intelligent?", "Are you creative?", "Are you ambitious?", "Are you patient?", "Do you work hard?", "Are you pure-blood?"];
-var points = 1;
+var points = 0;
 var i = 0;
 
 function pointsdisp(points){
     if(points >= 8 ){
         $("h1").text("Gryffindor");
+        $("body").css('background-color', 'red');
     }
     else if(points >= 6){
         $("h1").text("Hufflepuff");
@@ -37,8 +38,12 @@ $(document).ready(function () {
             points++;
         }
         if (i == qarray.length) {
-            $(".buttons-div").hide();
-            $("#submit-but").show();
+            $("#yes-button").click(function(){
+                points++;
+                $(".buttons-div").hide();
+                $("#submit-but").show();
+                $("h2").text("Click on submit for house reveal!");
+            });
         }
     });
 
@@ -48,14 +53,19 @@ $(document).ready(function () {
             i++;
         }
         if (i == qarray.length) {
-            $(".buttons-div").hide();
-            $("#submit-but").show();
+            $("#yes-button").click(function(){
+                $(".buttons-div").hide();
+                $("#submit-but").show();
+                $("h2").text("Click on submit for house reveal!");
+            });
         }
     });
 
     $("#submit-but").click(function(){
-        $(".body-wrapper").hide();
+        $("h2").text("You belong to:");
+        $(".buttons-div").hide();
         pointsdisp(points);
+        $(".body-wrapper").hide();
         $(".revealer").show();
     });
 });
