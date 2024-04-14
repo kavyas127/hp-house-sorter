faaccf2ae2f41c16993faceea4529ebac0002387
kavyas127 <officialkesai315@gmail.com>
2024-04-14 16:50:32 +0530

    commit 3

--- home.js
+++ home.js
@@ -1,12 +1,28 @@
-var qarray = ["Are you courageous?", "Are you a good leader?", "Are you kind?","Are you loyal?","Are you intelligent?","Are you creative?","Are you ambitious?","Are you patient?","Do you work hard?","Are you pure-blood?"]
+var qarray = ["Are you courageous?", "Are you a good leader?", "Are you kind?","Are you loyal?","Are you intelligent?","Are you creative?","Are you ambitious?","Are you patient?","Do you work hard?","Are you pure-blood?"];
+var points = -1;
+var i = 0;
 
-var counter = -1;
+$(".revealer").hide();
 
 $(document).ready(function(){
-    for(var i = 0; i<qarray.length; i++){
-            $("#yes-button").click(function(){
+    $("#yes-button").click(function(){
+        if(i < qarray.length){
             $("h2").text(qarray[i]);
-            counter++;
-        });
-    }
+            i++;
+            points++;
+        }
+    });
+
+    $("#no-button").click(function(){
+        if(i < qarray.length){
+            $("h2").text(qarray[i]);
+            i++;
+        }
+    });
 });
+
+$(".body-wrapper").hide();
+
+
+
+
