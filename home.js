eec07d5f6dd1691144cef89c6dec9dc09e8796fa
kavyas127 <officialkesai315@gmail.com>
2024-04-14 15:45:45 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,2 @@
+var qarray = ["Are you courageous?", "Are you a good leader?", "Are you kind?","Are you loyal?","Are you intelligent?","Are you creative?","Are you ambitious?","Are you patient?","Do you work hard?","Are you pure-blood?"]
+
