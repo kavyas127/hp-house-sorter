67ba9a48e2f366145b13ec8e2db2d11fb2face88
kavyas127 <officialkesai315@gmail.com>
2024-04-14 16:11:20 +0530

    commit 2

--- home.js
+++ home.js
@@ -1,2 +1,12 @@
 var qarray = ["Are you courageous?", "Are you a good leader?", "Are you kind?","Are you loyal?","Are you intelligent?","Are you creative?","Are you ambitious?","Are you patient?","Do you work hard?","Are you pure-blood?"]
 
+var counter = -1;
+
+$(document).ready(function(){
+    for(var i = 0; i<qarray.length; i++){
+            $("#yes-button").click(function(){
+            $("h2").text(qarray[i]);
+            counter++;
+        });
+    }
+});
